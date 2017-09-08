b0d635cf25152b27c216a0d5191c4796797d6509
Hao Zhu <haozhu233@gmail.com>
2017-09-08 16:53:57 -0400

    updates

--- R/handout_lite.R
+++ R/handout_lite.R
@@ -1,7 +1,17 @@
+#' Handout (lite) ver
+#'
+#' @param toc logical value for table of contents. See ?rmarkdown::pdf_document
+#' for details
+#' @param number_sections logical value for wether sections should be numbered.
+#' See ?rmarkdown::pdf_document for details
+#' @param latex_engine LaTeX engine. See ?rmarkdown::pdf_document for details
+#' @param ... other options to be passed to rmarkdown::pdf_document. See
+#' ?rmarkdown::pdf_document for details
+#'
 #' @importFrom rmarkdown pdf_document
 #' @export
 handout_lite <- function(toc = TRUE, number_sections = TRUE,
-                         highlight = "haddock", latex_engine = "xelatex",
+                         latex_engine = "xelatex",
                          ...) {
   template <- system.file("rmarkdown/templates/handout_lite/resources/template.tex",
                           package = "ifar.reports")
@@ -10,7 +20,6 @@ handout_lite <- function(toc = TRUE, number_sections = TRUE,
     latex_engine = latex_engine,
     template = template,
     number_sections = number_sections,
-    highlight = highlight,
     toc = toc,
     ...
   )
